88d79c44e6cd2affea933c785c13a4c5fcabfb1d
SK4MIL <kamil10078@gmail.com>
2022-03-29 11:16:51 +0200

    Added list of availible sorting function names.

--- Laboratorium 2/defines.h
+++ Laboratorium 2/defines.h
@@ -1,15 +1,16 @@
 
 #define N_MAX 65000
-#define N_RUNS 10
 
-#define FUNKCJA heapSort
+#define N_RUNS 10
+// Dostępne funkcje: bbSort;  insertsort; qsort; mixedBucket; heapSort;
+#define FUNKCJA mixedBucket
 //                  Z jakiego pliku pobierać dane
 #define INPUT_FILE "data/liczby.txt"
 //      Czy zapisać posortowaną tablice. 
 #define SAVE 0
 //                  W jakim pliku zapisać posortowane dane
-#define SORTED_FILE "data/posortowane_heap.txt"
+#define SORTED_FILE "data/posortowane_bucket2.txt"
 //                  Gdzie zapisać zmierzone czasy.
-#define TIME_FILE "time/heapTime.csv"
+#define TIME_FILE "time/bucketTime2.csv"
 
 
